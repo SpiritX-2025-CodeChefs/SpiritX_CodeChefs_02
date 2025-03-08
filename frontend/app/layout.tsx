419db204a68f0209222609aaa071dae6b9d6e3c5
Yasoga Nanayakkarawasam <yasogan6@outlook.com>
2025-03-09 00:12:54 +0530

    Add HeroUIProvider to layout and configure Tailwind CSS for HeroUI

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { HeroUIProvider } from "@heroui/react";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,11 +24,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className='dark'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <HeroUIProvider>
+          {children}
+        </HeroUIProvider>
       </body>
     </html>
   );
